92500b2112f7a2c93fff0c2b8c39845bfd711f96
gorkem <yurtseven.gorkem@gmail.com>
2023-05-11 10:24:54 -0700

    Update and rename setup_cuda.py to setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     ext_modules=[cpp_extension.CUDAExtension(
         'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu']
     )],
-    cmdclass={'build_ext': cpp_extension.BuildExtension}
+    cmdclass={'build_ext': cpp_extension.BuildExtension},
+    install_requires=['torch']
 )
